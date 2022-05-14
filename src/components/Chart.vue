74d7db028ea76f5ca0b5cce609f7699c0901d27d
Daniel Turcich <github@danielTurcich.com>
2022-05-13 20:41:51 -0400

    more drag and drop changes

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -4,51 +4,40 @@
 
 import { type Ref, ref } from 'vue'
 import { type AlbumSearchResult } from '../types'
-// import { AlbumSearchResult } from '../types'
 
 const showText = ref(false)
 const chartSize = { length: 3, height: 3 }
 const albumArray: Ref<AlbumSearchResult[]> = ref([])
 
-// const blah = [ ['cock', 'cock2'], ['cock', 'cock2']]
-
-console.log(100, albumArray.value)
-
 for (let x = 0; x < chartSize.height * chartSize.length; x++) {
 	albumArray.value.push({
-		artist: '',
-		name: '',
-		image:
-			'https://cdn.media.amplience.net/s/hottopic/11750610_hi?$productMainDesktop$'
+		artist: 'George Clanton',
+		name: 'Slide',
+		image: 'https://upload.wikimedia.org/wikipedia/en/4/41/Slide_GC.png'
 	})
 }
-
-console.log(101, albumArray.value)
-
-// function onMouseUp(event: MouseEvent) {
-// 	console.log(event)
-// }
 
 function onDragOver(dragEvent: DragEvent) {
 	try {
 		dragEvent.preventDefault()
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		dragEvent.dataTransfer!.dropEffect = 'copy'
+		dragEvent.dataTransfer!.dropEffect = 'move'
 	} catch {
 		//
 	}
 }
 
-function onDrop(dragEvent: DragEvent) {
+function onDrop(dragEvent: DragEvent, index: number) {
 	try {
 		dragEvent.preventDefault()
 		const data = dragEvent.dataTransfer?.getData('text/plain')!
 		const albumDraggedIn = JSON.parse(data) as AlbumSearchResult
 		const currrentElement = dragEvent.currentTarget as HTMLImageElement
 
-		currrentElement.src = albumDraggedIn.image
+		albumArray.value[index] = albumDraggedIn
 
-		// dragEvent.target!.appendChild(document.getElementById(data))
+		currrentElement.src = albumDraggedIn.image
+		currrentElement.alt = `${albumDraggedIn.artist} - ${albumDraggedIn.name}`
 
 		console.log(currrentElement)
 		console.log(albumDraggedIn)
@@ -56,23 +45,31 @@ function onDrop(dragEvent: DragEvent) {
 		//
 	}
 }
+
+function onDragStart(dragEvent: DragEvent, index: number) {
+	dragEvent.dataTransfer!.setData(
+		'text/plain',
+		JSON.stringify(albumArray.value[index])
+	)
+	dragEvent.dataTransfer!.dropEffect = 'copy'
+}
 </script>
 
 <template>
 	<div class="flex justify-center items-center">
-		<!-- <div>{{ props.currentDraggedAlbum.artist }}</div> -->
-
-		<!-- the actual chart -->
 		<div class="flex flex-wrap">
 			<img
 				v-for="(album, index) in albumArray"
 				:key="index"
 				width="150"
+				height="150"
 				:src="`${album.image}`"
 				:alt="`${album.artist} - ${album.name}`"
 				class="flex-[0_0_33%]"
+				draggable="true"
+				@dragstart="(dragEvent) => onDragStart(dragEvent, index)"
 				@dragover="onDragOver"
-				@drop="onDrop"
+				@drop="(dragEvent) => onDrop(dragEvent, index)"
 			/>
 		</div>
 
