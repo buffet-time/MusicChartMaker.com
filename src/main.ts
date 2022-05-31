0f3dc6ac34d3f221b883400b9522bf8f1fa3bd54
Daniel Turcich <github@danielTurcich.com>
2022-05-31 16:36:54 -0400

    Version set to 0.1.0 and added metatag to reflect version to client

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './tailwind.css'
+import Package from '../package.json'
+
+const meta = document.createElement('meta')
+meta.name = 'SiteVersion'
+meta.content = Package.version
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+document.getElementsByTagName('head')[0]!.appendChild(meta)
 
 createApp(App).mount('#app')
